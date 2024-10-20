63789b57889929e7932c6e57daa2020a2ec472cf
RoshanDahal123 <dahalroshan765@gmail.com>
2024-10-20 15:58:55 +0545

    get api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import axios from "axios";
 
 export default function Home() {
+  const [blogs, setBlogs] = useState([]);
   const fetchBlogs = async () => {
     const response = await axios.get("http://localhost:3000/blog/");
+    setBlogs(response.data.data);
   };
   useEffect(() => {
     fetchBlogs();
   }, []);
+  console.log(blogs);
   return (
     <>
       <Navbar />
       <div className="flex flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {blogs.map((blog) => {
+          return <Card blog={blog} key={blog._id} />;
+        })}
       </div>
     </>
   );
