14df6da0a4afdaf49e3264ef5a28ee04daca8b84
RoshanDahal123 <dahalroshan765@gmail.com>
2024-10-20 14:21:19 +0545

    fetched blog api from backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
+import axios from "axios";
 
 export default function Home() {
+  const fetchBlogs = async () => {
+    const response = await axios.get("http://localhost:3000/blog/");
+  };
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
   return (
     <>
       <Navbar />
-      <div className="flex-wrap">
+      <div className="flex flex-wrap">
         <Card />
         <Card />
         <Card />
