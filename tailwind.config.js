966647f38da24694df5524d101a8c97836e46cc7
RoshanDahal123 <dahalroshan765@gmail.com>
2024-10-22 13:26:22 +0545

    create blog UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
