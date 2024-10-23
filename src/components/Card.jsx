93faf09bb1a06452fe15822a6e19d81a2bb774b5
RoshanDahal123 <dahalroshan765@gmail.com>
2024-10-23 14:48:40 +0545

    updated in creteblog

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,11 +7,7 @@ function Card({ blog }) {
       <Link to={`/blog/${blog._id}`}>
         <div className="flex px-3 py-3">
           <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src={"http://localhost:3000/" + blog.image}
-              alt={blog.image}
-            />
+            <img className="w-full" src={blog.image} alt={blog.image} />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{blog.title}</div>
               <p className="text-gray-700 text-base">{blog.description}</p>
